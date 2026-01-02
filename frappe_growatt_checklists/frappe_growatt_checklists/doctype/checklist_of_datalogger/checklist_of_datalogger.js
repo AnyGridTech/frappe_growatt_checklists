b9c6e791152c4a098a9c1dd48a6a7f63636a8ead
Marco <gasparmarco92@gmail.com>
2026-01-02 13:40:55 +0000

    Refactor Initial Analysis and Related Doctypes
    - Updated form event handlers in `FormEvents.ts` for `checklist_of_datalogger` to improve field management and data retrieval.
    - Removed references to `inanly_docname` in `checklist_of_ev_charger`, `checklist_of_inverter`, `checklist_of_smart_energy_manager`, and `checklist_of_smart_meter` JSON files, replacing them with `ticket_docname` for consistency.
    - Enhanced `fields_handler` functions in `checklist_of_inverter` and `checklist_of_datalogger` to utilize `get_target_field_by_field_match` for fetching values from related doctypes.
    - Adjusted the creation of Serial No documents in `initial_analysis.js` to use `ticket_docname` instead of `inanly_docname`.
    - Cleaned up JSON definitions across multiple checklists to remove unnecessary fields and ensure proper linkage to the ticketing system.

--- frappe_growatt_checklists/frappe_growatt_checklists/doctype/checklist_of_datalogger/checklist_of_datalogger.js
+++ frappe_growatt_checklists/frappe_growatt_checklists/doctype/checklist_of_datalogger/checklist_of_datalogger.js
@@ -3,8 +3,49 @@
 "use strict";
 (() => {
   // frappe_growatt_checklists/doctype/checklist_of_datalogger/ts/FormEvents.ts
-  frappe.ui.form.on(cur_frm.doc.doctype, "setup", async () => {
-    await agt.checklist.setup();
-    await agt.corrections_tracker.run.run();
+  frappe.ui.form.on(cur_frm.doc.doctype, {
+    setup: async () => {
+      await agt.checklist.setup();
+      await agt.corrections_tracker.run.run();
+    },
+    onload: async (form) => {
+      fields_listener(form);
+    },
+    refresh: async (form) => {
+      fields_listener(form);
+    },
+    validate: async (form) => {
+      fields_listener(form);
+    }
   });
+  function fields_listener(form) {
+    fields_handler(form);
+    Object.keys(form.fields_dict).forEach((fn) => {
+      const field = form.fields_dict[fn];
+      if (field && field.df) {
+        field.df["onchange"] = () => {
+          fields_handler(form);
+        };
+      }
+    });
+  }
+  async function fields_handler(form) {
+    const main_eqp_purchase_invoice = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_purchase_invoice");
+    const main_eqp_has_battery = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_has_battery");
+    const main_eqp_has_sem = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_has_sem");
+    const main_eqp_has_sm = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_has_sm");
+    const main_eqp_has_neutral = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_has_neutral");
+    const main_eqp_has_transformer = await agt.utils.get_target_field_by_field_match(form, "Initial Analysis", "ticket_docname", "ticket_docname", "main_eqp_has_transformer");
+    const valuesToSet = {
+      main_eqp_purchase_invoice,
+      main_eqp_has_battery,
+      main_eqp_has_sem,
+      main_eqp_has_sm,
+      main_eqp_has_neutral,
+      main_eqp_has_transformer
+    };
+    Object.entries(valuesToSet).forEach(([field, value]) => {
+      form.set_value(field, value);
+    });
+  }
 })();
--- frappe_growatt_checklists/frappe_growatt_checklists/doctype/checklist_of_datalogger/ts/FormEvents.ts
+++ frappe_growatt_checklists/frappe_growatt_checklists/doctype/checklist_of_datalogger/ts/FormEvents.ts
@@ -1,9 +1,66 @@
-/*
-* This file handles the form events for the Initial Analysis doctype.
-* Read more: https://docs.frappe.io/framework/user/en/api/form#form-events
-*/
+import { ChecklistDatalogger } from "@anygridtech/frappe-agt-types/agt/doctype";
+import { FrappeForm } from "@anygridtech/frappe-types/client/frappe/core";
 
-frappe.ui.form.on(cur_frm.doc.doctype, 'setup', async () => {
-  await agt.checklist.setup();
-  await agt.corrections_tracker.run.run();
-}); 
+
+// frappe.ui.form.on(cur_frm.doc.doctype, 'onload', async () => {
+//   await growatt.checklists.run();
+//   await growatt.corrections_tracker.run();
+// });
+
+// const allowRoles = frappe.user.has_role(['Information Technology User', 'Administrator', 'System Manager']);
+
+// frappe.ui.form.on(cur_frm.doc.doctype, 'setup', async () => {
+//     await agt.setup.run();
+// });
+
+frappe.ui.form.on(cur_frm.doc.doctype, {
+  setup: async () => {
+    await agt.checklist.setup();
+    await agt.corrections_tracker.run.run();
+  },
+  onload: async (form: FrappeForm<ChecklistDatalogger>) => {
+    fields_listener(form);
+  },
+  refresh: async (form: FrappeForm<ChecklistDatalogger>) => {
+    fields_listener(form);
+  },
+  validate: async (form: FrappeForm<ChecklistDatalogger>) => {
+    fields_listener(form);
+  },
+});
+
+function fields_listener(form: FrappeForm<ChecklistDatalogger>) {
+  fields_handler(form);
+  Object.keys(form.fields_dict).forEach((fn) => {
+    const field = form.fields_dict[fn];
+    if (field && field.df) {
+      field.df['onchange'] = () => {
+        fields_handler(form);
+      };
+    }
+  });
+}
+
+async function fields_handler(form: FrappeForm<ChecklistDatalogger>) {
+
+  // Take values from other doctypes for general uses, such as hiding/showing fields
+  const main_eqp_purchase_invoice = await agt.utils.get_target_field_by_field_match(form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_purchase_invoice' );
+  const main_eqp_has_battery = await agt.utils.get_target_field_by_field_match(form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_has_battery' );
+  const main_eqp_has_sem = await agt.utils.get_target_field_by_field_match( form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_has_sem' );
+  const main_eqp_has_sm = await agt.utils.get_target_field_by_field_match( form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_has_sm' );
+  const main_eqp_has_neutral = await agt.utils.get_target_field_by_field_match( form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_has_neutral' );
+  const main_eqp_has_transformer = await agt.utils.get_target_field_by_field_match( form, 'Initial Analysis', 'ticket_docname', 'ticket_docname', 'main_eqp_has_transformer' );
+// Now set the values taken once in the form collectively
+  const valuesToSet = {
+    main_eqp_purchase_invoice,
+    main_eqp_has_battery,
+    main_eqp_has_sem,
+    main_eqp_has_sm,
+    main_eqp_has_neutral,
+    main_eqp_has_transformer
+  };
+  
+  Object.entries(valuesToSet).forEach(([field, value]) => {
+    form.set_value(field, value);
+  });
+}
